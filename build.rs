0881e29c61ff1f4becffa9d267a5d5915a7f3703
Andrey Shigantsov <shigantsov.a.a@gmail.com>
2021-03-19 14:46:41 +0600

    build profile used now

--- build.rs
+++ build.rs
@@ -3,10 +3,14 @@ extern crate git2 as git;
 
 
 fn main() {
+    let profile = std::env::var("PROFILE").unwrap();
     let dir = std::env::var("OUT_DIR").unwrap() + "/librtmp";
     let url = "https://github.com/Stargateur/librtmp";
     let _ = git::Repository::clone(url, &dir);
-    let dst = cmake::Config::new(&dir).build_target("rtmp").build();
+    let dst = cmake::Config::new(&dir)
+    .profile(profile.as_str())
+    .build_target("rtmp")
+    .build();
 
     if cfg!(any(windows)) {
         println!("cargo:rustc-link-search=native={}/build/Release", dst.display())

9c6745037368ce9b58360221a3d4b8eed76242e9
Andrey Shigantsov <shigantsov.a.a@gmail.com>
2021-03-19 16:22:10 +0600

    CRYPTO defined for default

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 extern crate cmake;
 extern crate git2 as git;
 
-
 fn main() {
     let profile = std::env::var("PROFILE").unwrap();
     let dir = std::env::var("OUT_DIR").unwrap() + "/librtmp";
     let url = "https://github.com/Stargateur/librtmp";
+    
     let _ = git::Repository::clone(url, &dir);
+    
     let dst = cmake::Config::new(&dir)
     .profile(profile.as_str())
+    .define("CRYPTO", "")
     .build_target("rtmp")
     .build();
 
